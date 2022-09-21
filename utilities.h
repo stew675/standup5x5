f5f97bd57c23025645bd93d6e2dd304add04d7f8
Stew Forster <stew675@gmail.com>
2022-09-21 07:43:57 -0500

    Made use of _pext_u32() order independent

--- utilities.h
+++ utilities.h
@@ -36,6 +36,7 @@ static struct frequency {
 	uint32_t	tm4;		// Tiered Mask 4
 	uint32_t	tm5;		// Tiered Mask 5
 	uint32_t	tm6;		// Tiered Mask 6
+	uint32_t	tmm;		// Logical OR of tm1..tm4
 	int		ready;		// Ready to set up
 	int		b;		// char - 'a'
 	struct tier	*sets;
@@ -61,7 +62,6 @@ static volatile int	num_readers	__attribute__ ((aligned(64))) = 0;
 // Put all general global variables together on their own CPU cache line
 static int32_t	min_search_depth __attribute__ ((aligned(64))) = 0;
 static uint32_t hash_collisions = 0;
-static uint32_t	tm_mask = 0;
 static int	write_metrics = 0;
 static int	nthreads = 0;
 static int	nkeys = 0;
@@ -669,21 +669,12 @@ by_frequency_hi(const void *a, const void *b)
 
 #ifdef __BMI2__
 
-// "aeious" are the best static defaults.  These must be
-// in alphabetical order for _pext_u32() to work properly
-uint32_t tm1 = 1 << ('a' - 'a');
-uint32_t tm2 = 1 << ('e' - 'a');
-uint32_t tm3 = 1 << ('i' - 'a');
-uint32_t tm4 = 1 << ('o' - 'a');
-uint32_t tm5 = 1 << ('s' - 'a');
-uint32_t tm6 = 1 << ('u' - 'a');
-
 // The sequence of instructions here is intended.  It achieves good
 // concurrency in the CPU by engaging both the ALU and AGU, which
-// is why f->tm5/tm6 is faster than using the global variables
+// is why f->tm5/tm6/tmm is faster than if using global variables
 #define CALCULATE_SET_AND_END						\
 	do {								\
-		struct tier *t = f->sets + _pext_u32(mask, tm_mask);	\
+		struct tier *t = f->sets + _pext_u32(mask, f->tmm);	\
 		uint32_t mf = !!(mask & f->tm5);			\
 		uint32_t ms = !(mask & f->tm6);				\
 		uint32_t off = t->toff3 + (ms * t->tlen3);		\
@@ -693,15 +684,6 @@ uint32_t tm6 = 1 << ('u' - 'a');
 	} while (0)
 
 #else
-
-// "uaeios" is the best static default order in
-// the absence of the _pext_u32() intrinsic
-uint32_t tm1 = 1 << ('u' - 'a');
-uint32_t tm2 = 1 << ('a' - 'a');
-uint32_t tm3 = 1 << ('e' - 'a');
-uint32_t tm4 = 1 << ('i' - 'a');
-uint32_t tm5 = 1 << ('o' - 'a');
-uint32_t tm6 = 1 << ('s' - 'a');
 
 // Determine the values for set and end in a branchless manner
 // The !! means we end up with only 0 or 1
@@ -727,10 +709,22 @@ setup_tkeys(struct frequency *f)
 {
 	struct tier	*t0 = f->sets;
 	uint32_t	*kp = t0->s + t0->l + NUM_POISON;
+	uint32_t	tm1, tm2, tm3, tm4, tmm = f->tmm;
 	uint32_t	*ks, len;
 	uint32_t	masks[16];
 
-	// XXX - Come up with a way to make this a loop
+	// We need to use the order that _pext_u32() will use
+	tm1 = 1 << __builtin_ctz(tmm);
+	tmm &= tmm - 1;
+	tm2 = 1 << __builtin_ctz(tmm);
+	tmm &= tmm - 1;
+	tm3 = 1 << __builtin_ctz(tmm);
+	tmm &= tmm - 1;
+	tm4 = 1 << __builtin_ctz(tmm);
+	tmm &= tmm - 1;
+	assert(tmm == 0);
+
+	// Define the mask bitmaps for splitting the sets
 	masks[0]  = 0;
 	masks[1]  = tm1;
 	masks[2]  = tm2;
@@ -749,6 +743,7 @@ setup_tkeys(struct frequency *f)
 	masks[14] = tm4 | tm3 | tm2;
 	masks[15] = tm4 | tm3 | tm2 | tm1;
 
+	// Create key arrays for each tier set mask
 	for (uint32_t mask, i = 1; i < 16; i++) {
 		struct tier *ts = f->sets + i;
 		mask = masks[i];
@@ -804,14 +799,14 @@ set_tier_offsets(struct frequency *f)
 	if (f == frq)
 		goto set_tier_offsets_done;
 
-	// "aeious" are the best static defaults
-	// These must be in alphabetical order
-	f->tm1 = tm1;
-	f->tm2 = tm2;
-	f->tm3 = tm3;
-	f->tm4 = tm4;
-	f->tm5 = tm5;
-	f->tm6 = tm6;
+	// "uaeios" are the best static defaults
+	f->tm1 = 1 << ('u' - 'a');
+	f->tm2 = 1 << ('a' - 'a');
+	f->tm3 = 1 << ('e' - 'a');
+	f->tm4 = 1 << ('i' - 'a');
+	f->tm5 = 1 << ('o' - 'a');
+	f->tm6 = 1 << ('s' - 'a');
+	f->tmm = (f->tm1 | f->tm2 | f->tm3 | f->tm4);
 
 	// Organise full set into 2 subsets, that which
 	// has tm5 followed by that which does not
@@ -955,7 +950,6 @@ main(int argc, char *argv[])
 	// Copy in the default file-name
 	strcpy(file, "words_alpha.txt");
 
-	tm_mask = (tm1 | tm2 | tm3 | tm4);
 	nthreads = get_nthreads();
 
 	if (argc > 1) {
